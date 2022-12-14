f2fb53b92a9e56cacbdb3f6512d4b6d7c4a8d30d
gemalas69 <93607571+gemalas69@users.noreply.github.com>
2022-12-14 18:36:24 +0200

    list comprehension

--- 2020/pagrindine/2020U1.py
+++ 2020/pagrindine/2020U1.py
@@ -58,9 +58,7 @@ def main():
     if geliu_sk < 1 or geliu_sk > 30:
         return
 
-    dienos = {}
-    for i in range(1, 93):
-        dienos[i] = 0
+    dienos = {diena: 0 for diena in range(0, 93)}
 
     for line in duomenys:
         pr_menesis = int(line[5:6])
