8a69bcb73399554f02b6169b56eb99c3274c2034
gemalas69 <93607571+gemalas69@users.noreply.github.com>
2022-12-14 18:07:05 +0200

    Komentarai, pakeitimai kaip rasomi rezultatai

--- 2017/pagrindine/2017U1.py
+++ 2017/pagrindine/2017U1.py
@@ -1,4 +1,5 @@
 def nustatyti(pirmas, antras):
+    # Sukuriamas zodynas, kuriame rodoma, kaip koduojami skaiciai i raides
     sesioliktainis = {10: 'A',
     11: 'B',
     12: 'C',
@@ -6,15 +7,14 @@ def nustatyti(pirmas, antras):
     14: 'E',
     15: 'F'}
 
-    for v in sesioliktainis:
-        if pirmas == v:
-            pirmas_skaitmuo = sesioliktainis[v]
-        if antras == v:
-            antras_skaitmuo = sesioliktainis[v]
-    if pirmas == 0:
-        pirmas_skaitmuo = 0
-    if antras == 0:
-        antras_skaitmuo = 0
+    # Einama per zodyno skaicius, jei duoti skaiciai sutampa, rezultato skaitmuo tampa atitinkama raide
+    for reiksme in sesioliktainis:
+        if pirmas == reiksme:
+            pirmas_skaitmuo = sesioliktainis[reiksme]
+        if antras == reiksme:
+            antras_skaitmuo = sesioliktainis[reiksme]
+    
+    # Jeigu skaiciai yra intervale nuo 0 iki 9, jie islieka tokie patys
     if pirmas in range(10):
         pirmas_skaitmuo = pirmas
     if antras in range(10):
@@ -24,38 +24,64 @@ def nustatyti(pirmas, antras):
 
 
 def gauti_sesioliktaini_skaiciu(rgb):
-    res = ''
+    # Sukuriamas tuscias rezultato string, i kuri bus dedami atskiri sesioliktainio skaiciaus skaitmenys
+    rez = ''
+
+    # Gauti skaiciai paverciami i integer tipa
     rgb = [int(sk) for sk in rgb.split(' ')]
 
+    # Einama per kiekviena spalva, kiekvienas numeris paverciamas i sesioliktainio skaiciaus skaitmenis pagal salyga
+    # Naudojamas // operatorius gauti skaiciaus sveikaja dali ir % operatorius, kad gauti liekana
     for spalva in rgb:
         pirmas_skaitmuo, antras_skaitmuo = nustatyti(spalva // 16, spalva % 16)
-        res += pirmas_skaitmuo + antras_skaitmuo
-    
-    return res
+
+        # Gauti skaitmenys prijungiami i rezultato string
+        rez += pirmas_skaitmuo + antras_skaitmuo
+
+    return rez
 
 
 def main():
+    # Perskaitomi pradiniai duomenys i duomenu masyva. Perskaitomas ilgis bei plotis ir jie pasalinami is duomenu masyvo
     with open('./2017/pagrindine/U1.txt', 'r') as f:
         duomenys = [line.strip() for line in f.readlines()]
         ilgis, plotis = (int(sk) for sk in duomenys[0].split(' '))
         duomenys.pop(0)
     
+    # Tikrinama, ar ilgis ir plotis atitinka salyga
     assert 1<=ilgis<=10000
     assert 1<=plotis<=10000
 
-    ilgio = []
-    plocio = []
+    # Sukuriami tusti masyvai, kuriuose bus laikomi rezultatai
+    skaiciai = []
+    rezultatas = ''
 
-    for rgb in duomenys[:3]:
+    # Einama per visus duotus skaicius, jei paverciami i sesioliktainius ir sudedami i masyva
+    for rgb in duomenys:
         sesioliktainis = gauti_sesioliktaini_skaiciu(rgb)
-        ilgio.append(sesioliktainis)
-    for rgb in duomenys[3:]:
-        sesioliktainis = gauti_sesioliktaini_skaiciu(rgb)
-        plocio.append(sesioliktainis)
+        skaiciai.append(sesioliktainis)
+
+    # Formatuojamas rezultatas, kad butu tiek eiluciu ir tiek stulpeliu, koks yra plotis ir ilgis
+    for _ in range(ilgis):
+
+        # I kiekviena stulpeli sudedamas toks skaicius sesioliktainiu skaiciu, koks duotas plotis duomenu faile        
+        rezultatas += ';'.join(skaiciai[:plotis])
+
+        # Jeigu jau kazkas irasyta i rezultata, dedama nauja eilute, kad atskirti stulpelius
+        if rezultatas:
+            rezultatas += '\n'
+        
+        # Jau surasyti skaiciai yra pasalinami, o jei nera ka salinti sugaunama ir praleidziama IndexError klaida
+        for i in range(plotis):
+            try:
+                skaiciai.pop(i)
+            except IndexError:
+                pass
     
+    # Irasomas gautas rezultatas
     with open('./2017/pagrindine/U1rez.txt', 'w') as f:
-        f.write(f'{ilgio[0]};{ilgio[1]};{ilgio[2]}\n')
-        f.write(f'{plocio[0]};{plocio[1]};{plocio[2]}')
+        f.write(rezultatas)
 
 
-main()
+if __name__ == '__main__':
+    main()
