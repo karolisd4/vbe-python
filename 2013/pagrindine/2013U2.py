26eadbcabdc4b935c3d6d505aca16f34a78672fb
Karolis <93607571+karolisd4@users.noreply.github.com>
2023-02-10 09:18:15 +0200

    Komentarai, naujas rikiavimas

--- 2013/pagrindine/2013U2.py
+++ 2013/pagrindine/2013U2.py
@@ -1,47 +1,68 @@
 def main():
     with open('./2013/pagrindine/U2.txt', 'r') as f:
-        lines = f.readlines()
-        lines = [line.strip() for line in lines]
-    lines.pop(0)
+        lines = [line.strip() for line in f.readlines()]    # Perskaitomi duomenys, pasalinami nereikalingi tarpai.
+    miestu_sk = int(lines[0])
+    assert 1<=miestu_sk<=103    # Tikrinama, ar miestu skaicius atitinka salyga.
+    lines.pop(0)    # Istrinamas pirmas elementas (miestu skaicius), nes jo nereikia.
 
+    # Sukuriamas masyvas ir zodynai duomenims saugoti.
     apskritys = []
     apskriciu_duom = {}
     miestu_duom = {}
 
+    # Einama per kiekviena eilute, atitinkamose pozicijose randami miestu pavadinimai, apskriciu pavadinimai ir gyventoju skaiciai.
     for line in lines:
-        miestas, apskritis, gyventoju_sk = line.split(' ')
+        miestas = line[:20].strip()
+        apskritis = line[20:33].strip()
+        gyventoju_sk = line[33:].strip()
+        assert 100<=int(gyventoju_sk)<=600000   # Tikrinama, ar gyventoju skaicius atitinka salyga.
+        # Sudedami duomenys apie mietus ir apskritis i ju masyvus.
         miestu_duom[miestas] = int(gyventoju_sk)
         apskritys.append(apskritis)
 
+    # Is apskriciu masyvo pasalinami visi duplikatai, lieka tik unikalus elementai su set() funkcija.
     apskritys = set(apskritys)
     apskriciu_sk = str(len(apskritys))
 
+    # Einama per visas apskritis ir eilutes duomenyse.
     for apskritis in apskritys:
+        # Kiekvienai apskriciai atsikirai sukuriamas tuscias masyvas.
         apskrities_info = []
         for line in lines:
-            miestas, apskritis_line, gyventoju_sk = line.split(' ')
+            # Randama informacija apie miesta, apskriti.
+            miestas = line[:20].strip()
+            apskritis_line = line[20:33].strip()
+            gyventoju_sk = line[33:].strip()
             if apskritis == apskritis_line:
+                # Jeigu dabartine apskritis sutampa su apskritimi pirmame for cikle, i informacijos masyva idedamas atitinkamas miestas ir jo gyventoju skaicius.
                 apskrities_info.append(f'{miestas} {gyventoju_sk}')
-        apskriciu_duom[apskritis] = apskrities_info
+        # I apskriciu zodyna idedama apskritis ir jos informacijos masyvas su miestais ir ju gyventoju skaiciais.
+        apskriciu_duom.setdefault(apskritis, apskrities_info)
 
+    # Einama per apskritis ir ju informacijos masyvus.
     for apskritis in apskritys:
         gyventoju_info = []
         for value in apskriciu_duom[apskritis]:
+            # Randamas miestas ir jo gyventoju skaicius, kiekvieno miesto gyventoju skaiciai sudedami i masyva. 
             miestas, gyventoju_sk = value.split(' ')
             gyventoju_info.append(int(gyventoju_sk))
+        # I apskriciu duomenu masyva idedamas maziausias gyventoju skaicius is visu miestu ir gyventoju skaiciu suma.
         apskriciu_duom[apskritis] = min(gyventoju_info), sum(gyventoju_info)
-    gyventoju_info = []
 
-    for value in apskriciu_duom.values():
-        gyventoju_info.append(value[0])
-        gyventoju_info = sorted(gyventoju_info)
+    # Surikiuojamas duomenu zodynas didejimo tvarka. Jei gyventoju skaiciai vienodi, bus rikiuojama abeceliskai.
+    apskriciu_duom = sorted(apskriciu_duom.items(), key=lambda x: x[::-1])
 
+    # Surasomi rezultatai.
     with open('./2013/pagrindine/U2rez.txt', 'w') as f:
         f.write(f'{apskriciu_sk}\n')
-        for gyventoju_sk in gyventoju_info:
-            apskritis = [i for i in apskriciu_duom if apskriciu_duom[i][0]==gyventoju_sk][0]
-            gyventoju_sum = apskriciu_duom[apskritis][1]
-            f.write(f'{apskritis} {gyventoju_sk} {gyventoju_sum}\n')
+        for data in apskriciu_duom:
+            # Duomenu zodyne randama visa reikalinga informacija, naudojant indeksus.
+            pavadinimas = data[0]
+            min_gyv_sk = str(data[1][0])
+            gyv_sk = str(data[1][1])
+            # Pavadinimui duodama 13 poziciju.
+            f.write(f'{pavadinimas:<13} {min_gyv_sk} {gyv_sk}\n')
 
-
-main()
+# Pagrindine funkcija.
+if __name__ == '__main__':
+    main()
