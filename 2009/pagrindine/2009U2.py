230968707b9f50381b7e9852cffa25a4e21f9247
Karolis <93607571+karolisd4@users.noreply.github.com>
2023-03-01 08:42:10 +0200

    Update 2009U2.py

--- 2009/pagrindine/2009U2.py
+++ 2009/pagrindine/2009U2.py
@@ -7,12 +7,13 @@ class Participant():
         self.time = time
 
 def write_result(result):
-    with open('./2009/pagrindine/U2rez.txt', 'w') as f:
-        f.write(result)
+    with open('U2rez.txt', 'w') as f:
+        for r in result:
+            f.write(f'{r.name} {r.time.minute} {r.time.second}\n')
 
 
 def main():
-    with open('./2009/pagrindine/U2.txt', 'r') as f:
+    with open('U2.txt', 'r') as f:
         lines = [line.strip() for line in f.readlines()]
     group_count = int(lines[0])
     lines.pop(0)
@@ -20,12 +21,14 @@ def main():
 
     participants = []
     groups = []
+    results = []
+
     for _ in range(group_count):
         groups.append([])
 
     group = 0
     for line in lines:
-        if len(line) == 1:
+        if len(line) <= 2:
             group += 1
             continue
         name = line[0:20].strip()
@@ -37,8 +40,19 @@ def main():
         for i in range(1, group_count+1):
             if participant.group == i:
                 groups[i-1].append(participant)
-    
 
+    for i in range(1, group_count+1):
+        amount = len(groups[i-1]) // 2
+        for j, person in enumerate(groups[i-1]):
+            if j == amount - 1:
+                continue
+            chosen = min(groups[i-1], key=lambda participant: participant.time)
+            results.append(chosen)
+            groups[i-1].remove(chosen)
+
+    results = sorted(results, key=lambda person: person.time)
+    write_result(results)
+        
 
 if __name__ == '__main__':
     main()
